33c3997015ed98f446a5c5656f11a6bbcb184bb0
gsk <155007612+Rskholgade@users.noreply.github.com>
2024-02-14 19:39:28 +0530

    Add files via upload
    hi

---
+++ Patilkaki/patil.js
@@ -0,0 +1,56 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+// Countdown Timer
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    const tenMinutes = 60 * 10,
+        display = document.getElementById('timer');
+    startTimer(tenMinutes, display);
+};
+
+// Form that slides up after 15 seconds
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        document.getElementById('signup-form').classList.remove('hidden');
+    }, 15000);
+});
